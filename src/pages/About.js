b2c92ffbeea3ba560c3dfdd7511aa4bca2ffc79f
danielnathaniel <danielyaghoobian@gmail.com>
2023-01-09 13:37:37 -0800

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>This is an example of a cocktail menu/database. </p>
+    </section>
+  );
+}
